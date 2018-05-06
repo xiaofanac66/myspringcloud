922c9b6d8c7e58619efeb9afcfebed0a0634d766
qusifan <qusifan@huobi.com>
2018-05-06 18:08:15 +0800

    commit

--- helloclient/src/main/java/com/sf/helloclient/HelloClientApplication.java
+++ helloclient/src/main/java/com/sf/helloclient/HelloClientApplication.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
 public class HelloClientApplication {
     @Value("${server.port}")
     String port;
@@ -26,5 +30,9 @@ public class HelloClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloClientApplication.class, args);
+    }
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
     }
 }
--- service-robbin/src/main/java/com/sf/servicerobbin/ServiceRobbinApplication.java
+++ service-robbin/src/main/java/com/sf/servicerobbin/ServiceRobbinApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +16,11 @@ public class ServiceRobbinApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRobbinApplication.class, args);
+    }
+
+    @Bean
+    public AlwaysSampler defaultSampler(){
+        return new AlwaysSampler();
     }
 
     @Bean
